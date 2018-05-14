f994a1fc010dba30aa792128eaf69ecb7202d943
antoine250 <poteau@obs-vlfr.fr>
2018-05-14 09:13:44 +0200

    Update read_coriolis.R

--- read_coriolis.R
+++ read_coriolis.R
@@ -27,6 +27,17 @@ TRAJECTORY_PARAMETERS=get.var.ncdf(nc_file,"TRAJECTORY_PARAMETERS")
 JULD=get.var.ncdf(nc_file,"JULD")
 plot(JULD,ylim=c(23000,23100))
 
+timedec <- ((abs(JULD - trunc(JULD)) *24 ) )
+
+TIME_H <- format(strptime( paste(floor(timedec), round((timedec-floor(timedec))*60), sep=":"), format="%H:%M"),"%H")
+TIME_M <- format(strptime( paste(floor(timedec), round((timedec-floor(timedec))*60), sep=":"), format="%H:%M"),"%H")
+TIME_Y <- format(as.Date(trunc(JULD), origin="1950-01-01 00:00:00 UTC"),"%Y")
+TIME_M <- format(as.Date(trunc(JULD), origin="1950-01-01 00:00:00 UTC"),"%M")
+TIME_D <- format(as.Date(trunc(JULD), origin="1950-01-01 00:00:00 UTC"),"%D")
+
+
+
+
 # latitude
 LATITUDE=get.var.ncdf(nc_file2,"LATITUDE")
 POSITION_QC=get.var.ncdf(nc_file2,"POSITION_QC")
